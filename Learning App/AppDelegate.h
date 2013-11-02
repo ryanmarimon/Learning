682a7b2712cba0713922606e04f6ca42c829c813
ryanmarimon <Ryan@Ryans-MacBook-Pro.local>
2013-11-02 00:47:00 -0400

    Initial Commit

---
+++ Learning App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Learning App
+//
+//  Created by Ryan Marimon on 11/2/13.
+//  Copyright (c) 2013 Ryan Marimon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Learning App/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Learning App
+//
+//  Created by Ryan Marimon on 11/2/13.
+//  Copyright (c) 2013 Ryan Marimon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
